38a9505f7a1479f48d6a9d21982af5efa0da0a1b
Dan Akmat <akmatdan@gmail.com>
2021-04-26 09:42:55 +0600

    TabItems

---
+++ Funflix/Views/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  Funflix
+//
+//  Created by Daniil Akmatov on 26/4/21.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text("Hello, Discover!")
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
---
+++ Funflix/Views/MoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesView.swift
+//  Funflix
+//
+//  Created by Daniil Akmatov on 25/4/21.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text("Hello, Movie!")
+    }
+}
+
+struct MoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesView()
+    }
+}
