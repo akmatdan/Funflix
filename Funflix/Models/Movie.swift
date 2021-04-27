ef63d4d85407e395883a5b7f7f5ad80192c3b3b2
Dan Akmat <akmatdan@gmail.com>
2021-04-27 17:43:00 +0600

    New model Movie
    objects, posterPatch fixing, voteAvarage for circle, title with language (title (lang))

---
+++ Funflix/Models/Movie.swift
@@ -0,0 +1,46 @@
+//
+//  Movie.swift
+//  Funflix
+//
+//  Created by Daniil Akmatov on 27/4/21.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    var result: [Movie]
+}
+
+struct Movie: Codable, Identifiable {
+    var id: Int?
+    var title: String?
+    var original_language: String?
+    var overview: String?
+    var poster_path: String?
+    var backdrop_path: String?
+    var popularity: Double?
+    var vote_avarage: Double?
+    var vote_count: Int?
+    var video: Bool?
+    var adult: Bool?
+    var release_data: String?
+    
+    var posterPath: String {
+        if let path = poster_path {
+            return "hhtp://image.tmdb.org/t/p/original/\(path)"
+        }
+        return ""
+    }
+    
+    var voteAverage: Double {
+        if let avg = vote_avarage {
+            return avg / 10.0
+        }
+        return 0.0
+    }
+    
+    var titleWithLanguage: String {
+        guard let title = title, let lang = original_language else { return "" }
+        return "\(title)(\(lang))"
+    }
+}
