fc670e698fc0b4fd23cb7f5ad79d5687d782922b
Dan Akmat <akmatdan@gmail.com>
2021-04-28 13:44:52 +0600

    AsyncImageLoader
    It's publish image of UIImage type.

---
+++ Funflix/AsyncImage/ImageLoader.swift
@@ -0,0 +1,13 @@
+//
+//  ImageLoader.swift
+//  Funflix
+//
+//  Created by Daniil Akmatov on 28/4/21.
+//
+
+import Combine
+import UIKit
+
+final class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+}
