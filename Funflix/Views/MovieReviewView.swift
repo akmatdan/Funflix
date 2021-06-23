05f9b5906d4d9ebfb6a49f4af06979a7594eea04
Dan Akmat <akmatdan@gmail.com>
2021-06-23 18:40:53 +0600

    Created MovieReviewView

---
+++ Funflix/Views/MovieReviewView.swift
@@ -0,0 +1,15 @@
+//
+//  MovieReviewView.swift
+//  Funflix
+//
+//  Created by Daniil Akmatov on 23/6/21.
+//
+
+import SwiftUI
+
+struct MovieReviewView: View {
+    
+    var movie: Movie
+    
+    @ObservedObject var movieReview: MovieReviewManager
+}
